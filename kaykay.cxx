422d754d1f18fb277ed2b1c948698cf7d55e9eb7
project-Upinado <meupinado@gmail.com>
2021-03-25 15:40:25 +0800

    prject-Upinado

---
+++ kaykay.cxx
@@ -0,0 +1,8 @@
+#include <iostream>
+int main()
+{
+	std::cout << " FACIAL RECOGNITION SYSTEM \n\n ";
+	std::cout << " A facial recognition system is a technology capable of matching a human face from a digital image or a video frame against a database of faces, typically employed to authenticate users through ID verification services, works by pinpointing and measuring facial features from a given image.";
+	return 0;
+	
+}
